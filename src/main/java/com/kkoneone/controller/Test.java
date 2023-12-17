9a0425cfd45ac362da1e897b5093992baa00f1df
kkoneone11 <604065124@qq.com>
2023-12-17 16:10:19 +0800

    测试1

---
+++ src/main/java/com/kkoneone/controller/Test.java
@@ -0,0 +1,12 @@
+package com.kkoneone.controller;
+
+/**
+ * @Author：kkoneone11
+ * @name：Test
+ * @Date：2023/12/17 16:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
